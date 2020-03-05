475aac9ea9b5c2f2e2ce904a072c39ea51c15184
Henrik Tunedal <tunedal@gmail.com>
2020-03-05 23:26:02 +0100

    Add simple web page template

--- plots.py
+++ plots.py
@@ -1,6 +1,10 @@
 # https://plot.ly/python/map-configuration/
 # https://plot.ly/python/choropleth-maps/
 
+import sys, time, re
+from pathlib import Path
+from io import StringIO
+
 import plotly.graph_objects as go
 import plotly.express as px
 from pandas import DataFrame
@@ -8,7 +12,7 @@ from pandas import DataFrame
 import stats
 
 
-def plot_stats_on_map(excluded_countries=[]):
+def plot_stats_on_map(outfile, excluded_countries=[]):
     excluded_countries = set(excluded_countries)
 
     data = list(r for r in stats.case_density()
@@ -26,13 +30,54 @@ def plot_stats_on_map(excluded_countries=[]):
         locations="iso_alpha",
         color="density",
         hover_name="country",
+        height=1000,
         color_continuous_scale=px.colors.sequential.YlOrRd)
-    fig.show()
+
+    fig.write_html(file=outfile, full_html=False, auto_open=False)
 
 
-if __name__ == "__main__":
+def make_map():
     excluded_countries=[     # extreme outliers throwing off the scale
         "San Marino",
     ]
-    plot_stats_on_map(excluded_countries)
 
+    # outfile = Path(__file__).parent / "map.html"
+    # with outfile.open("w") as f:
+    #     plot_stats_on_map(f, excluded_countries)
+
+    buf = StringIO()
+    plot_stats_on_map(buf, excluded_countries)
+    return buf.getvalue()
+
+
+def process_template(template, outfile, replacements):
+    if isinstance(replacements, dict):
+        replacements = replacements.items()
+    line_map = {f"<!-- INSERT {rid.upper()} -->\n": data
+                for rid, data in replacements}
+    for line in template:
+        if line in line_map:
+            outfile.write(line_map[line])
+        else:
+            outfile.write(line)
+
+
+def main():
+    map_data = make_map()
+
+    replacements = {
+        "map": map_data,
+        "date": time.strftime("%Y-%m-%d %H:%M:%S")
+    }
+
+    if len(sys.argv) == 1:
+        process_template(sys.stdin, sys.stdout, replacements)
+    elif len(sys.argv) == 2:
+        with open(sys.argv[1], "r") as f:
+            process_template(f, sys.stdout, replacements)
+    else:
+        raise Exception("Bad argument count")
+
+
+if __name__ == "__main__":
+    main()
